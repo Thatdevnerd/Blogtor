0a7deeb837dd46fe8023c095c624832c97699f1f
rayan1159 <baskruithof999@gmail.com>
2024-06-12 12:39:38 +0200

    Added ability to generate blog post via command

---
+++ src/Command/GenerateBlogPostCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Blogs;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'generate:blog-post',
+    description: 'Generate a new blog post.',
+)]
+class GenerateBlogPostCommand extends Command
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em
+    )
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('title', InputArgument::REQUIRED, 'The title of the blog post.')
+            ->addArgument('content', InputArgument::REQUIRED, 'The content of the blog post.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $blog = new Blogs();
+
+        $title = $input->getArgument('title');
+        $content = $input->getArgument('content');
+
+        if (!$title || !$content) {
+            $io->error('Title and content are required.');
+            return Command::FAILURE;
+        }
+
+        $blog->setTitle($title);
+        $blog->setContent($content);
+        $blog->setDate(new \DateTime());
+
+        $this->em->persist($blog);
+        $this->em->flush();
+
+        $io->success('The blog post has been generated');
+
+        return Command::SUCCESS;
+    }
+}
--- src/Command/GenerateTestUserCommand.php
+++ src/Command/GenerateTestUserCommand.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -20,8 +19,8 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class GenerateTestUserCommand extends Command
 {
     public function __construct(
-        private EntityManagerInterface $em,
-        private UserPasswordHasherInterface $passwordHasher
+        private readonly EntityManagerInterface $em,
+        private readonly UserPasswordHasherInterface $passwordHasher
     )
     {
         parent::__construct();
