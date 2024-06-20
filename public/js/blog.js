bff893bf0aa00bb1736d443d5f6b98891d44c2a1
rayan1159 <baskruithof999@gmail.com>
2024-06-20 20:04:44 +0200

    working on some nice ui effects

--- public/js/blog.js
+++ public/js/blog.js
@@ -2,14 +2,41 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardCount = 0;
     let noPosts = false;
     let cardCollection = document.getElementsByClassName('blog-card');
+    let cardCollectionLength = cardCollection.length;
 
-    for(let i = 0; i < cardCollection.length; i++) {
-        const targetSpanInner = cardCollection[i].getElementsByTagName("span")[0].innerHTML;
-        if (targetSpanInner.includes("No posts found")) {
-            noPosts = true
-        } else {
-            cardCount++;
+    const countCards = () => {
+        for(let i = 0; i < cardCollectionLength; i++) {
+            const targetSpanInner = cardCollection[i].getElementsByTagName("span")[0].innerHTML;
+            if (targetSpanInner.includes("No posts found")) {
+                noPosts = true
+            } else {
+                cardCount++;
+            }
         }
     }
-    console.log(cardCount, noPosts ? "No posts found" : "Posts found");
+
+    const cardsOnEnterAnimation = () => {
+        for(let i = 0; i < cardCollectionLength; i++) {
+            cardCollection[i].style.animation = `fadeIn 0.5s ${i * 0.1}s forwards`;
+        }
+        setTimeout((() => {
+            for(let i = 0; i < cardCollectionLength; i++) {
+                cardCollection[i].style.animation = 'none';
+            }
+        }, 3 * 1000));
+    }
+
+    const preventInitialCardLoad = () => {
+        if (noPosts) {
+            cardCollection[0].style.display = 'none';
+        }
+    }
+
+    (async() => {
+        countCards();
+        cardsOnEnterAnimation();
+        preventInitialCardLoad();
+    })();
+
+    console.log(cardCount)
 });
