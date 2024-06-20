59d456e06a33a2a7ef7c142049b9b1874d1b0a65
rayan1159 <baskruithof999@gmail.com>
2024-06-20 15:43:25 +0200

    blog.js now detects if there are posts added

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     let cardCount = 0;
+    let noPosts = false;
     let cardCollection = document.getElementsByClassName('blog-card');
 
     for(let i = 0; i < cardCollection.length; i++) {
-        console.log(cardCollection[i]);
+        const targetSpan = cardCollection[i].getElementsByTagName("span")[0];
+        if (cardCollection[i].getElementsByTagName("span")[0].innerHTML.includes("No posts found")) {
+            noPosts = true
+        }
         cardCount++;
     }
-
-    console.log(cardCount);
+    console.log(cardCount, noPosts ? "No posts found" : "Posts found");
 });
