1b2490a7e4b5118c4886b3003720be4dbf028858
rayan1159 <baskruithof999@gmail.com>
2024-06-20 15:21:37 +0200

    Gonna mix in some javascript on blog overview

---
+++ public/js/blog.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cardCount = 0;
+    let card = document.getElementsByClassName('.blog-card');
+
+    for(let i = 0; i < card.length; i++) {
+        card = card + 1;
+    }
+
+    console.log(cardCount);
+});
