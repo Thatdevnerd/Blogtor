c020000fd6ac411d839b9db117eb01872cb05f21
rayan1159 <baskruithof999@gmail.com>
2024-06-21 14:07:44 +0200

    Working on blog.js again

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     let noPosts = false;
     let cardCollection = document.getElementsByClassName('blog-card');
+    let cardParent = document.getElementsByClassName("blog-card-wrapper")[0];
     let cardCollectionLength = cardCollection.length;
+
+
+    const DEFAULT_PAGE = 1;
+
+    let cardHistoryObj = [{}];
+    let cardObj = [{}];
 
     const countCards = () => {
         let cardCount = 0;
@@ -26,33 +33,60 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const renderPaginationButtons = () => {
         for (let i = 0; i < cardCollectionLength; i++) {
-            i = i++
-            i = i.toString()
+            i = i++;
             const btn = document.createElement('button');
-            btn.setAttribute('data-page', i);
-            btn.innerHTML = i;
+            btn.setAttribute('data-page', i.toString());
+            btn.innerHTML = i.toString();
             document.getElementById('pagination');
         }
     }
 
-    const drawPaginatedCards = (page) => {
-        const paginationCalc = paginationCalc(cardCollection, 3, page);
-        const cardArray = paginationCalc[0];
+    const renderPaginatedCards = (page) => {
+        const calc = paginationCalc(cardCollection, 3, page);
+        const cardArray = calc.array;
+
+        for (let i = 0; i < cardArray.length; i++) {
+            const card = cardArray[i];
+        }
     }
 
     document.addEventListener('click', (e) => {
-        if (e.target instanceof HTMLButtonElement) {
-            const clickedBtn = e.target;
-            const pageNumber = parseInt(clickedBtn.getAttribute('data-page'));
-            const page = paginationCalc(cardCollection, 3, pageNumber);
-            console.log(page);
+        const clickedBtn = e.target;
+        const pageNumber = parseInt(clickedBtn.getAttribute('data-page'));
+        const page = paginationCalc(cardCollection, 3, pageNumber)
+
+
+        if (countCards() >= 3) {
+            const bodyRect = document.body.getBoundingClientRect()
+            const elemRect = page.array[0].getBoundingClientRect();
+
+            for(let i = 0; i < page.array.length; i++) {
+                cardObj.push({
+                    [1]: {
+                        [i]: {
+                            element: page.array[i],
+                            top: elemRect.top - bodyRect.top,
+                            left: elemRect.left - bodyRect.left,
+                            width: page.array[i].offsetWidth,
+                            height: page.array[i].offsetHeight
+                        }
+                    }
+                })
+                console.log('loading in card', page.array[i], 'card obj', cardObj);
+            }
+
+            let cardToRender = document.createElement('div');
+            // cardParent.appendChild(cardToRender);
+            cardParent.appendChild(cardObj[1][1]);
         }
     });
 
-    //TODO Render pagination buttons based on number of cards
-
+    /**
+     * IIFE to render paginated cards and pagination buttons
+     */
     (async() => {
         if (countCards() > 0) {
+            renderPaginatedCards(DEFAULT_PAGE);
             renderPaginationButtons();
         }
     })();
